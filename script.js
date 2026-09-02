bf55964b4dcbbc6372f392b359bab723670a8428
Soledad <solmmiranda6@gmail.com>
2026-09-02 02:18:57 -0300

    agregamos tarjetas de proyectos al portfolio con js

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ function cambiar_modo(){
     return;
 }
 
-const tecnologias = ["html", "css", "javascript", "mysql", "java", "c++"];
+const tecnologias = ["html", "css", "javascript", "mysql", "java", "c++"]; //array: guarda una lista de cosas
 const contenedor_tecnologias = document.querySelector(".tecnologias-lista");
 
 //parametros: 1° elemento, 2° indice del elemento
@@ -36,4 +36,46 @@ tecnologias.forEach((tecnologia, index) => { // por cada elemento del array tecn
     span.textContent = tecnologia;
 
     contenedor_tecnologias.appendChild(span);
+});
+
+const proyecto = { //objeto: describe una cosa es una variable. Ej: nombre: Javscript, descripcion: tecnologia muy usada, nivel: intermedio, etc.
+    nombre: "Muma cosmetics",
+    descripcion: "tienda online de cosméticos hechos con IA.",
+    tecnologias: ["HTML", "CSS", "Javascript"]
+}; 
+
+const proyecto2 = {
+    nombre: "Turnero",
+    descripcion: "turnero para la gestion de turnos de una veterinaria",
+    tecnologias: ["PHP", "CSS", "Javascript"]
+}; 
+
+const proyectos = [proyecto, proyecto2];
+const contenedor_proyectos = document.querySelector(".lista-proyectos");
+
+proyectos.forEach((proyecto, index)=> {
+    const tarjeta = document.createElement("div");
+    const titulo = document.createElement("h3");
+    const descripcion = document.createElement("p");
+    const cont_tec_proyectos = document.createElement("div");
+
+    tarjeta.classList.add("proyecto");
+    titulo.textContent = proyecto.nombre;
+    descripcion.textContent = proyecto.descripcion;
+
+    tarjeta.appendChild(titulo);
+    tarjeta.appendChild(descripcion);
+    contenedor_proyectos.appendChild(tarjeta);
+
+    proyecto.tecnologias.forEach((tecnologia => {
+        const span = document.createElement("span");
+
+        span.textContent = tecnologia;
+
+        cont_tec_proyectos.appendChild(span);
+        cont_tec_proyectos.classList.add("tecnologias-proyecto");
+        tarjeta.appendChild(cont_tec_proyectos);
+    }))
+
+
 });
